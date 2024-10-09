079e4ab36895ec4213e1a6c26c9ff2235ca827a2
damhyeong <rhdwhdals8@naver.com>
2024-10-09 18:38:48 +0900

    saving Working Files. at Week8-2

--- routes/likes.js
+++ routes/likes.js
@@ -3,33 +3,20 @@ const router = express.Router();
 
 const {param, validationResult} = require("express-validator")
 
+const {
+    addLike,
+    removeLike
+} = require("../controller/LikeController")
 
 router.use(express.json());
-
-const validate = (req, res, next) => {
-    const err = validationResult(req);
-
-    return err.isEmpty() ? next() : res.status(400).json(err.array());
-}
 
 router
     .route("/:bookId")
     .post(
-        [
-            param("bookId").notEmpty().isInt(),
-            validate
-        ],
-        (req, res) => {
-        const {bookId} = req.params;
-    })
-    .delete(
-        [
-            param("bookId").notEmpty().isInt(),
-            validate
-        ],
-        (req, res) => {
-        const {bookId} = req.params;
-    })
+        addLike
+    ).delete(
+        removeLike
+    )
 
 
 module.exports = router;
