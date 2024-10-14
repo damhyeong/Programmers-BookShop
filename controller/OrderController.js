1f8dee36ec8ac609c34b0345b1cb31b6d9ca3594
damhyeong <rhdwhdals8@naver.com>
2024-10-15 01:50:50 +0900

    Commit at Week9 Part2 - and Complete chaining 3 Queries.

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,65 +1,67 @@
-const conn = require("../mariadb");
+const connection = require("../mariadb");
 
 const {StatusCodes} = require("http-status-codes");
 
-const addOrder = (req, res) => {
-    const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
+const addOrder = async (req, res) => {
+    const conn = await connection();
 
-    let delivery_id;
+    const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
 
     let sql = "INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)";
     let values = [delivery["address"], delivery["receiver"], delivery["contact"]];
 
-    conn.query(
-        sql,
-        values,
-        (err, results) => {
-            if(err){
-                console.log(err);
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
-            }
-            delivery_id = results.insertId;
+    let delivery_id;
 
-        }
-    )
+    try{
+        const [results] = await conn.query(
+            sql,
+            values
+        )
+        delivery_id = results.insertId
+    } catch (err) {
+        console.log(err);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    }
+    console.log("delivery_id : "  + delivery_id)
+
 
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`;
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
 
     let order_id;
 
-    conn.query(
-        sql,
-        values,
-        (err, results) => {
-            if(err){
-                console.log(err);
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
-            }
-
-            order_id = results.insertId;
-
-        }
-    )
+    try{
+        const [results] = await conn.query(
+            sql,
+            values
+        );
+        order_id = results.insertId;
+    } catch (err) {
+        console.log(err);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    }
 
     sql = "INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?";
     values = [];
+
     items.forEach((value, index) => {
         values.push([order_id, value["book_id"], value["quantity"]])
     })
 
-    conn.query(
-        sql,
-        [values],
-        (err, results) => {
-            if(err){
-                console.log(err);
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
-            }
+    try{
+        const [results] = await conn.query(
+            sql,
+            [values]
+        );
 
-            return res.status(StatusCodes.OK).json(results);
-        }
-    )
+        return res.status(StatusCodes.OK).json(results);
+    } catch (err) {
+        console.log(err);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    }
+
+
+    await conn.end();
 }
 
 const getOrders = (req, res) => {
--- mariadb.js
+++ mariadb.js
@@ -1,11 +1,15 @@
-const mariadb = require("mysql2");
+const mariadb = require("mysql2/promise");
 
-const conn = mariadb.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "root",
-    database : "Bookshop",
-    dateStrings : true
-});
+const conn = async() => {
+    const conn = await mariadb.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "Bookshop",
+        dateStrings: true
+    });
+
+    return conn;
+}
 
 module.exports = conn;
